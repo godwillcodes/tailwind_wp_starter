6b7c859009c485999b7fbf964e52c8accc71a975
Godwill Barasa <33608094+godwillcodes@users.noreply.github.com>
2025-09-17 15:47:41 +0300

    Add tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.php',
+    './**/*.html',
+    './**/*.js',
+    './template-parts/**/*.php',
+    './inc/**/*.php',
+    './pages/**/*.php'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
